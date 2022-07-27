450f9f0277840cd53cef52e1d25c404d5ea84dfc
gabyrmg <100819516+gabyrmg@users.noreply.github.com>
2022-07-26 22:16:16 -0300

    Creacion carpeta BackEnd

---
+++ Portfolio_GabrielaGonzalez/BackEnd/src/main/java/com/portfolio/grmg/GrmgApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.grmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrmgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrmgApplication.class, args);
+	}
+
+}
---
+++ Portfolio_GabrielaGonzalez/BackEnd/src/test/java/com/portfolio/grmg/GrmgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.grmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrmgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
